09cad53bab605b2ee218b8d5eb6974aaead19291
Sankpal5354 <hsankpal738@gmail.com>
2026-05-28 21:29:02 +0530

    Added MySQL database integration

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 import requests
+import mysql.connector
 
 app = FastAPI()
+
+# MySQL Connection
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="student",
+    database="weather_project"
+)
+
+cursor = db.cursor()
 
 @app.get("/")
 def home():
@@ -18,9 +29,36 @@ def get_weather(city: str):
 
     current = data["current_condition"][0]
 
+    temperature = current["temp_C"]
+    humidity = current["humidity"]
+    weather = current["weatherDesc"][0]["value"]
+
+    # Insert into MySQL
+    query = """
+    INSERT INTO weather_history
+    (city, temperature, humidity, weather)
+    VALUES (%s, %s, %s, %s)
+    """
+
+    values = (city, temperature, humidity, weather)
+
+    cursor.execute(query, values)
+
+    db.commit()
+
     return {
         "city": city,
-        "temperature": current["temp_C"],
-        "humidity": current["humidity"],
-        "weather": current["weatherDesc"][0]["value"]
+        "temperature": temperature,
+        "humidity": humidity,
+        "weather": weather
     }
+
+
+@app.get("/history")
+def history():
+
+    cursor.execute("SELECT * FROM weather_history")
+
+    rows = cursor.fetchall()
+
+    return {"history": rows}
